635a85a2c6e33c2df206237c3da583641dd2e23d
HornyFerret <ombro.de.morto@gmail.com>
2021-07-11 17:14:32 +0300

    Добавьте файлы проекта.

---
+++ codewars-7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace codewars_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "George";
+            string w = Greet(name);
+            Console.WriteLine(w);
+        }
+        public static string Greet(string name)
+        {
+            string soon = $"Hello, {name} how are you doing today?";
+            return soon;
+        }
+    }
+}
